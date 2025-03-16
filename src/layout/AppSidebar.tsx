5f299cdd683dce0a75c2e0c40bcd11564245aca9
Hamidreza-Taghaddos <armantaghaddos@gmail.com>
2025-03-16 15:19:27 +0330

    home content removed

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -20,7 +20,7 @@ const navItems: NavItem[] = [
   {
     icon: <GridIcon />,
     name: "Dashboard",
-    subItems: [{ name: "Ecommerce", path: "/", pro: false }],
+    subItems: [{ name: "Home", path: "/", pro: false }],
   },
 ];
 
